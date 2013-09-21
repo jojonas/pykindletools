20638a000ba66fb7d16f5c5c3e44ffa48f47340a
jojonas <lieb.jonas@gmail.com>
2013-09-21 16:33:21 +0200

    final version with native mobi writer, will now use kindlegen

--- webapis/reddit.py
+++ webapis/reddit.py
@@ -1,6 +1,7 @@
 import praw
 import util
 import datetime
+import collections
 
 class RedditBook(util.HTMLBook):
 	def __init__(self, username):
@@ -17,14 +18,20 @@ class RedditSubredditBook(RedditBook):
 		self.subreddit = subreddit
 		self.count = count
 		self.username = username
+		self.posts = collections.OrderedDict()
 		
 	def gather(self):
 		self.addHeading("/r/%s" % self.subreddit)
 		nowStr = datetime.datetime.now().strftime("%B %d, %Y, %H:%M")
 		self.addHtml("<sup>(compiled by /u/%s on %s)</sup>" % (self.username, nowStr))
 		submissions = self.reddit.get_subreddit(self.subreddit).get_hot(limit=self.count)
-		for submission in submissions:
+		for i, submission in enumerate(submissions):
 			self.addPagebreak()
+			
+			aname = "post%d" % i
+			self.posts[aname] = submission.title
+			self.addHtml("<a name='%s'></a>" % aname)
+			
 			self.addHeading(submission.title, 2)
 			self.addHtml("<sup>(score: %d, submitted by /u/%s)</sup>" % (submission.score, submission.author.name))
 			if submission.is_self:
@@ -33,4 +40,4 @@ class RedditSubredditBook(RedditBook):
 					html = html.replace(" class=\"md\"", "")
 					self.addHtml(html)
 			else:
-				self.addParagraph("<a href=\"%s\">%s</a>" % (submission.url, submission.url), indent="10px")
+				self.addParagraph("<a href=\"%s\">%s</a>" % (submission.url, submission.url))
--- webapis/util.py
+++ webapis/util.py
@@ -2,15 +2,21 @@ import xml.dom.minidom
 import re
 import HTMLParser
 
+
+def walkTree(root, function):
+	function(root)
+	if root.childNodes:
+		for node in root.childNodes:
+			if node.nodeType == node.ELEMENT_NODE:
+				walkTree(node, function)
+				
 class HTMLBook:
 	def __init__(self):
 		self.document = xml.dom.minidom.Document()
 		self._declLength = len(self.document.toxml())
 		self.html = self.document.createElement('html')
 		self.head = self.document.createElement('head')
-		self.guide = self.document.createElement('guide')
 		self.body = self.document.createElement('body')
-		self.head.appendChild(self.guide)
 		self.document.appendChild(self.html)
 		self.html.appendChild(self.head)
 		self.html.appendChild(self.body)
@@ -31,9 +37,17 @@ class HTMLBook:
 		self.body.appendChild(imported)
 		
 	def toHtml(self):
+		def rmAttribs(node):
+			for attrib in ("id", "class"):
+				if node.hasAttribute(attrib):
+					node.removeAttribute(attrib)
+					
+		walkTree(self.document.documentElement, rmAttribs)
+		
 		html = self.document.toxml()[self._declLength:]
-		html = re.sub(r">\s*<",r"><", html) 				# remove whitespace
-		html = re.sub(r"<([^/>]+)/>", r"<\1></\1>", html) 	# remove self closing
+		html = re.sub(r'>\s*<',r'><', html) 				# remove whitespace
+		html = re.sub(r'[\t\n\r\f\v]',r' ', html) 				# remove all whitespace that is not a space
+		html = re.sub(r'<([^/>]+)/>', r'<\1></\1>', html) 	# remove self closing
 		# http://stackoverflow.com/questions/97522/what-are-all-the-valid-self-closing-elements-in-xhtml-as-implemented-by-the-maj
 		allowed_selfclose = ["area", "base", "br", "col", "embed", "hr", "img", "input", "keygen", "link", "menuitem", "meta", "param", "source", "track", "wbr"]
 		allowed_selfclose += ["basefont", "bgsound", "frame", "isindex"]
