eb0ae958a005023e3c9638996fef1b9d2356d41e
jojonas <lieb.jonas@gmail.com>
2013-09-24 02:14:48 +0200

    module loading now dynamically (but kinda hackish, if this will be cleaned up some time, "I would be so happy...")

--- pykindle/webapis/__init__.py
+++ pykindle/webapis/__init__.py
@@ -1,5 +1,29 @@
-import module_evernote		as evernote
-import module_reddit 		as reddit
-import module_rss			as rss
-import module_wikipedia 	as wikipedia
-import module_wordpress		as wordpress
+import importlib
+import sys
+import os, os.path
+
+PREFIX = 'module_'
+
+currentDir = os.path.dirname(os.path.realpath(__file__))
+
+loaded = {}
+for fname in os.listdir(currentDir):
+	fullfname = os.path.join(currentDir, fname)
+	if os.path.isfile(fullfname):
+		moduleName, ext = os.path.splitext(fname)
+		if fname.startswith(PREFIX) and ext.lower() in ['.pyc', '.py']:
+			moduleName = moduleName[len(PREFIX):]
+			if not moduleName in loaded:
+				fullName = "{pkg}.{name}".format(pkg=__name__, name=moduleName)
+				realName = ".module_{name}".format(name=moduleName)
+				try:
+					loaded[moduleName] = sys.modules[fullName] = importlib.import_module(
+						realName, 
+						package=__name__
+					)
+				except ImportError as e:
+					print "Module '{name}' will be unavailable:".format(name=moduleName), e
+				
+				
+globals().update(loaded)
+		
